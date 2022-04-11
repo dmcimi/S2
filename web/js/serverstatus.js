5da4e417fda2f18ed6203b5a45c31d30aa539dfc
dmcimi <dmcimi@outlook.jp>
2022-04-11 19:19:24 +0800

    first commit

---
+++ web/js/serverstatus.js
@@ -0,0 +1,175 @@
+(async () => {
+    let stats = await (await fetch("json/stats.json")).json()
+    for (let i = 0; i < stats.servers.length; i++) {
+        let node = document.createElement("div")
+        node.id += "table-item-" + i
+        node.className = "table-item"
+        document.querySelector(".list-inner").append(node)
+        if (stats.servers[i].online4 || stats.servers[i].online6) {
+            document.querySelector(`#table-item-${i}`).innerHTML = `<div class="node">
+    <img class="flag" src="https://cdn.jsdelivr.net/gh/wjy20030407/wjy/images/flags/square/${stats.servers[i].region}.svg" alt>
+    <div>
+        <div class="name">${stats.servers[i].name}</div>
+        <div class="location">${stats.servers[i].location}</div>
+    </div>
+</div>
+<div class="type">${stats.servers[i].type}</div>
+<div class="uptime">${stats.servers[i].uptime == "1 天" ? "1 Day" : stats.servers[i].uptime.replace(/天/, "Days")}</div>
+<div class="network">${byteConvert(stats.servers[i].network_tx)}↑ ${byteConvert(stats.servers[i].network_rx)}↓</div>
+<div class="traffic">${byteConvert(stats.servers[i].network_out)}↑ ${byteConvert(stats.servers[i].network_in)}↓</div>
+<div class="cpu">
+    <div class="progress">
+        <div style="width: ${Math.round(stats.servers[i].cpu)}%; background-color: ${progressConvert(Math.round(stats.servers[i].cpu))};" class="progress-bar">
+            <div>${Math.round(stats.servers[i].cpu)}%</div>
+        </div>
+    </div>
+</div>
+<div class="mem">
+    <div class="progress">
+        <div style="width: ${Math.round(stats.servers[i].memory_used / stats.servers[i].memory_total * 100)}%; background-color: ${progressConvert(Math.round(stats.servers[i].memory_used / stats.servers[i].memory_total * 100))}" class="progress-bar">
+            <div>${Math.round(stats.servers[i].memory_used / stats.servers[i].memory_total * 100)}%</div>
+        </div>
+    </div>
+</div>
+<div class="hdd">
+    <div class="progress">
+        <div style="width: ${Math.round(stats.servers[i].hdd_used / stats.servers[i].hdd_total * 100)}%; background-color: ${progressConvert(Math.round(stats.servers[i].hdd_used / stats.servers[i].hdd_total * 100))}" class="progress-bar">
+            <div>${Math.round(stats.servers[i].hdd_used / stats.servers[i].hdd_total * 100)}%</div>
+        </div>
+    </div>
+</div>
+<div class="status">
+    <div class="status-dot" style="background-color: ${Math.round(stats.servers[i].cpu) <= 70 ? "" : "#faae42"};"></div>
+    <div class="status-info">${Math.round(stats.servers[i].cpu) <= 70 ? "Available" : "Busy"}</div>
+</div>`
+            document.querySelector(`#table-item-${i}`).style.borderColor = Math.round(stats.servers[i].cpu) <= 70 ? "" : "#faae42"
+        } else {
+            document.querySelector(`#table-item-${i}`).innerHTML = `<div class="node">
+    <img class="flag" src="https://cdn.jsdelivr.net/gh/wjy20030407/wjy/images/flags/square/${stats.servers[i].region}.svg" alt>
+    <div>
+        <div class="name">${stats.servers[i].name}</div>
+        <div class="location">${stats.servers[i].location}</div>
+    </div>
+</div>
+<div class="type">${stats.servers[i].type}</div>
+<div class="uptime">Offline</div>
+<div class="network">-</div>
+<div class="traffic">-</div>
+<div class="cpu">
+    <div class="progress">
+        <div style="width: 100%; background-color: #e62965;" class="progress-bar">
+            <div>Offline</div>
+        </div>
+    </div>
+</div>
+<div class="mem">
+    <div class="progress">
+        <div style="width: 100%; background-color: #e62965;" class="progress-bar">
+            <div>Offline</div>
+        </div>
+    </div>
+</div>
+<div class="hdd">
+    <div class="progress">
+        <div style="width: 100%; background-color: #e62965;" class="progress-bar">
+            <div>Offline</div>
+        </div>
+    </div>
+</div>
+<div class="status">
+    <div class="status-dot" style="background-color: #a2a5b9;"></div>
+    <div class="status-info">Offline</div>
+</div>`
+            document.querySelector(`#table-item-${i}`).style.borderColor = "#e62965"
+        }
+    }
+})()
+
+setInterval(() => {
+    (async () => {
+        let stats = await (await fetch("json/stats.json")).json()
+        for (let i = 0; i < stats.servers.length; i++) {
+            try {
+                if (stats.servers[i].online4 || stats.servers[i].online6) {
+                    document.querySelector(`#table-item-${i}`).style.borderColor = Math.round(stats.servers[i].cpu) <= 70 ? "" : "#faae42"
+                    document.querySelector(`#table-item-${i} .flag`).src = `https://cdn.jsdelivr.net/gh/wjy20030407/wjy/images/flags/square/${stats.servers[i].region}.svg`
+                    document.querySelector(`#table-item-${i} .location`).textContent = stats.servers[i].location
+                    document.querySelector(`#table-item-${i} .type`).textContent = stats.servers[i].type
+                    document.querySelector(`#table-item-${i} .uptime`).textContent = stats.servers[i].uptime == "1 天" ? "1 Day" : stats.servers[i].uptime.replace(/天/, "Days")
+                    document.querySelector(`#table-item-${i} .network`).textContent = `${byteConvert(stats.servers[i].network_tx)}↑ ${byteConvert(stats.servers[i].network_rx)}↓`
+                    document.querySelector(`#table-item-${i} .traffic`).textContent = `${byteConvert(stats.servers[i].network_out)}↑ ${byteConvert(stats.servers[i].network_in)}↓`
+                    document.querySelector(`#table-item-${i} .cpu .progress-bar`).style.width = `${Math.round(stats.servers[i].cpu)}%`
+                    document.querySelector(`#table-item-${i} .cpu .progress-bar`).style.backgroundColor = progressConvert(Math.round(stats.servers[i].cpu))
+                    document.querySelector(`#table-item-${i} .cpu .progress-bar div`).textContent = `${Math.round(stats.servers[i].cpu)}%`
+                    document.querySelector(`#table-item-${i} .mem .progress-bar`).style.width = `${Math.round(stats.servers[i].memory_used / stats.servers[i].memory_total * 100)}%`
+                    document.querySelector(`#table-item-${i} .mem .progress-bar`).style.backgroundColor = progressConvert(Math.round(stats.servers[i].memory_used / stats.servers[i].memory_total * 100))
+                    document.querySelector(`#table-item-${i} .mem .progress-bar div`).textContent = `${Math.round(stats.servers[i].memory_used / stats.servers[i].memory_total * 100)}%`
+                    document.querySelector(`#table-item-${i} .hdd .progress-bar`).style.width = `${Math.round(stats.servers[i].hdd_used / stats.servers[i].hdd_total * 100)}%`
+                    document.querySelector(`#table-item-${i} .hdd .progress-bar`).style.backgroundColor = progressConvert(Math.round(stats.servers[i].hdd_used / stats.servers[i].hdd_total * 100))
+                    document.querySelector(`#table-item-${i} .hdd .progress-bar div`).textContent = `${Math.round(stats.servers[i].hdd_used / stats.servers[i].hdd_total * 100)}%`
+                    document.querySelector(`#table-item-${i} .status-dot`).style.backgroundColor = Math.round(stats.servers[i].cpu) <= 70 ? "" : "#faae42"
+                    document.querySelector(`#table-item-${i} .status-info`).textContent = Math.round(stats.servers[i].cpu) <= 70 ? "Available" : "Busy"
+                } else {
+                    document.querySelector(`#table-item-${i}`).style.borderColor = "#e62965"
+                    document.querySelector(`#table-item-${i} .flag`).src = `https://cdn.jsdelivr.net/gh/wjy20030407/wjy/images/flags/square/${stats.servers[i].region}.svg`
+                    document.querySelector(`#table-item-${i} .location`).textContent = stats.servers[i].location
+                    document.querySelector(`#table-item-${i} .type`).textContent = stats.servers[i].type
+                    document.querySelector(`#table-item-${i} .uptime`).textContent = "Offline"
+                    document.querySelector(`#table-item-${i} .network`).textContent = "-"
+                    document.querySelector(`#table-item-${i} .traffic`).textContent = "-"
+                    document.querySelector(`#table-item-${i} .cpu .progress-bar`).style.width = "100%"
+                    document.querySelector(`#table-item-${i} .cpu .progress-bar`).style.backgroundColor = "#e62965"
+                    document.querySelector(`#table-item-${i} .cpu .progress-bar div`).textContent = "Offline"
+                    document.querySelector(`#table-item-${i} .mem .progress-bar`).style.width = "100%"
+                    document.querySelector(`#table-item-${i} .mem .progress-bar`).style.backgroundColor = "#e62965"
+                    document.querySelector(`#table-item-${i} .mem .progress-bar div`).textContent = "Offline"
+                    document.querySelector(`#table-item-${i} .hdd .progress-bar`).style.width = "100%"
+                    document.querySelector(`#table-item-${i} .hdd .progress-bar`).style.backgroundColor = "#e62965"
+                    document.querySelector(`#table-item-${i} .hdd .progress-bar div`).textContent = "Offline"
+                    document.querySelector(`#table-item-${i} .status-dot`).style.backgroundColor = "#a2a5b9"
+                    document.querySelector(`#table-item-${i} .status-info`).textContent = "Offline"
+                }
+            } catch {
+                document.querySelector(`#table-item-${i}`).style.borderColor = "#e62965"
+                document.querySelector(`#table-item-${i} .uptime`).textContent = "Offline"
+                document.querySelector(`#table-item-${i} .network`).textContent = "-"
+                document.querySelector(`#table-item-${i} .traffic`).textContent = "-"
+                document.querySelector(`#table-item-${i} .cpu .progress-bar`).style.width = "100%"
+                document.querySelector(`#table-item-${i} .cpu .progress-bar`).style.backgroundColor = "#e62965"
+                document.querySelector(`#table-item-${i} .cpu .progress-bar`).textContent = "Offline"
+                document.querySelector(`#table-item-${i} .mem .progress-bar`).style.width = "100%"
+                document.querySelector(`#table-item-${i} .mem .progress-bar`).style.backgroundColor = "#e62965"
+                document.querySelector(`#table-item-${i} .mem .progress-bar`).textContent = "Offline"
+                document.querySelector(`#table-item-${i} .hdd .progress-bar`).style.width = "100%"
+                document.querySelector(`#table-item-${i} .hdd .progress-bar`).style.backgroundColor = "#e62965"
+                document.querySelector(`#table-item-${i} .hdd .progress-bar`).textContent = "Offline"
+                document.querySelector(`#table-item-${i} .status-dot`).style.backgroundColor = "#a2a5b9"
+                document.querySelector(`#table-item-${i} .status-info`).textContent = "Offline"
+            }
+        }
+    })()
+}, 3000);
+
+let byteConvert = (data) => {
+    if (data < 1024) {
+        return data.toFixed(0) + 'B'
+    } else if (data < 1024 * 1024) {
+        return (data / 1024).toFixed(0) + 'K'
+    } else if (data < 1024 * 1024 * 1024) {
+        return (data / 1024 / 1024).toFixed(1) + 'M'
+    } if (data < 1024 * 1024 * 1024 * 1024) {
+        return (data / 1024 / 1024 / 1024).toFixed(2) + 'G'
+    } else {
+        return (data / 1024 / 1024 / 1024 / 1024).toFixed(2) + 'T'
+    }
+}
+
+let progressConvert = (data) => {
+    if (data <= 70) {
+        return ""
+    } else if (data <= 90) {
+        return "#faae42"
+    } else {
+        return "#e62965"
+    }
+}
